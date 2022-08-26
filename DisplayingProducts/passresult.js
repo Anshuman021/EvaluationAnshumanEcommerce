d0c5c76b1d3e189b5682f302ddc64abf425477aa
Anshuman Rathore <anshuman.rathore@atcs.com>
2022-08-26 15:40:25 +0530

    Id has been added for each product

--- DisplayingProducts/passresult.js
+++ DisplayingProducts/passresult.js
@@ -3,9 +3,11 @@ window.addEventListener('load',()=>{
     const Prod_Name = localStorage.getItem('prod-name');
     const Prod_Price = localStorage.getItem('prod-price');
     const Prod_ImgUrl = localStorage.getItem('prod-imgurl');
+    const Prod_Id = localStorage.getItem('prod-id');
 
     document.getElementById('prod_name').innerHTML = Prod_Name;
     document.getElementById('prod_price').innerHTML = Prod_Price;
     document.getElementById('prod_imgurl').src = Prod_ImgUrl;
+    document.getElementById('prod-Id-showing').innerHTML = Prod_Id;
 
 })
--- DisplayingProducts/viewing.js
+++ DisplayingProducts/viewing.js
@@ -4,10 +4,12 @@ function cat_1_prod_1(){
     const Shoe1Name = document.getElementById('shoe1-name').innerText;
     const Shoe1Price = document.getElementById('shoe1-price').innerText;
     const Shoe1ImgUrl = document.getElementById('shoe1-ImgUrl').src;
+    const Shoe1Id = document.getElementsByClassName('prod-shoe1')[0].innerText;
     
     localStorage.setItem("prod-name",Shoe1Name);
     localStorage.setItem("prod-price",Shoe1Price);
     localStorage.setItem("prod-imgurl",Shoe1ImgUrl);
+    localStorage.setItem("prod-id",Shoe1Id);
 
     return;
 }
@@ -18,10 +20,12 @@ function cat_1_prod_2(){
     const Shoe2Name = document.getElementById('shoe2-name').innerText;
     const Shoe2Price = document.getElementById('shoe2-price').innerText;
     const Shoe2ImgUrl = document.getElementById('shoe2-ImgUrl').src;
+    const Shoe2Id = document.getElementsByClassName('prod-shoe2')[0].innerText;
 
     localStorage.setItem("prod-name",Shoe2Name);
     localStorage.setItem("prod-price",Shoe2Price);
     localStorage.setItem("prod-imgurl",Shoe2ImgUrl);
+    localStorage.setItem("prod-id",Shoe2Id);
 
 
     return;
@@ -33,10 +37,12 @@ function cat_1_prod_3(){
     const Shoe3Name = document.getElementById('shoe3-name').innerText;
     const Shoe3Price = document.getElementById('shoe3-price').innerText;
     const Shoe3ImgUrl = document.getElementById('shoe3-ImgUrl').src;
+    const Shoe3Id = document.getElementsByClassName('prod-shoe3')[0].innerText;
 
     localStorage.setItem("prod-name",Shoe3Name);
     localStorage.setItem("prod-price",Shoe3Price);
     localStorage.setItem("prod-imgurl",Shoe3ImgUrl);
+    localStorage.setItem("prod-id",Shoe3Id);
 
 
 
@@ -52,10 +58,12 @@ function cat_2_prod_1(){
     const Shirt1Name = document.getElementById('shirt1-name').innerText;
     const Shirt1Price = document.getElementById('shirt1-price').innerText;
     const Shirt1ImgUrl = document.getElementById('shirt1-ImgUrl').src;
+    const Shirt1Id = document.getElementsByClassName('prod-shirt1')[0].innerText;
 
     localStorage.setItem("prod-name",Shirt1Name);
     localStorage.setItem("prod-price",Shirt1Price);
     localStorage.setItem("prod-imgurl",Shirt1ImgUrl);
+    localStorage.setItem("prod-id",Shirt1Id);
 
     return;
 }
@@ -68,10 +76,12 @@ function cat_2_prod_2(){
     const Shirt2Name = document.getElementById('shirt2-name').innerText;
     const Shirt2Price = document.getElementById('shirt2-price').innerText;
     const Shirt2ImgUrl = document.getElementById('shirt2-ImgUrl').src;
+    const Shirt2Id = document.getElementsByClassName('prod-shirt2')[0].innerText;
 
     localStorage.setItem("prod-name",Shirt2Name);
     localStorage.setItem("prod-price",Shirt2Price);
     localStorage.setItem("prod-imgurl",Shirt2ImgUrl);
+    localStorage.setItem("prod-id",Shirt2Id);
 
     return;
 }
@@ -83,10 +93,13 @@ function cat_2_prod_3(){
     const Shirt3Name = document.getElementById('shirt3-name').innerText;
     const Shirt3Price = document.getElementById('shirt3-price').innerText;
     const Shirt3ImgUrl = document.getElementById('shirt3-ImgUrl').src;
+    const Shirt3Id = document.getElementsByClassName('prod-shirt3')[0].innerText;
+
 
     localStorage.setItem("prod-name",Shirt3Name);
     localStorage.setItem("prod-price",Shirt3Price);
     localStorage.setItem("prod-imgurl",Shirt3ImgUrl);
+    localStorage.setItem("prod-id",Shirt3Id);
 
     return;
 }
@@ -101,10 +114,14 @@ function cat_3_prod_1(){
     const Watch1Name = document.getElementById('watch1-name').innerText;
     const Watch1Price = document.getElementById('watch1-price').innerText;
     const Watch1ImgUrl = document.getElementById('watch1-ImgUrl').src;
+    const Watch1Id = document.getElementsByClassName('prod-watch1')[0].innerText;
+
 
     localStorage.setItem("prod-name",Watch1Name);
     localStorage.setItem("prod-price",Watch1Price);
     localStorage.setItem("prod-imgurl",Watch1ImgUrl);
+    localStorage.setItem("prod-id",Watch1Id);
+
 
     return;
 }
@@ -115,10 +132,13 @@ function cat_3_prod_2(){
     const Watch2Name = document.getElementById('watch2-name').innerText;
     const Watch2Price = document.getElementById('watch2-price').innerText;
     const Watch2ImgUrl = document.getElementById('watch2-ImgUrl').src;
+    const Watch2Id = document.getElementsByClassName('prod-watch2')[0].innerText;
 
     localStorage.setItem("prod-name",Watch2Name);
     localStorage.setItem("prod-price",Watch2Price);
     localStorage.setItem("prod-imgurl",Watch2ImgUrl);
+    localStorage.setItem("prod-id",Watch2Id);
+
 
     return;
 }
@@ -130,10 +150,13 @@ function cat_3_prod_3(){
     const Watch3Name = document.getElementById('watch3-name').innerText;
     const Watch3Price = document.getElementById('watch3-price').innerText;
     const Watch3ImgUrl = document.getElementById('watch3-ImgUrl').src;
+    const Watch3Id = document.getElementsByClassName('prod-watch3')[0].innerText;
+
 
     localStorage.setItem("prod-name",Watch3Name);
     localStorage.setItem("prod-price",Watch3Price);
     localStorage.setItem("prod-imgurl",Watch3ImgUrl);
+    localStorage.setItem("prod-id",Watch3Id);
 
     return;
 }
